e8e96edac944f200fd3266a4c32bfb4544678da2
Kalenga <kalengabwalya@gmail.com>
2018-04-17 12:49:59 +0200

    ..

--- js/script.js
+++ js/script.js
@@ -4,4 +4,10 @@ console.log("Global message: " + message);
 var a = function(){
     var message = "inside a";
     console.log("a message: " + message);
+    b();
+};
+
+function b(){
+    console.log("b message: " + message);
 }
+a();
